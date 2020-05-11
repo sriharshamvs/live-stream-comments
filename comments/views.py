c3be3fb08e974d069577018fcacb6d313e31d9be
Ganesh <ganesh@swecha.net>
2020-05-11 12:31:07 +0200

    Secure WS

--- comments/views.py
+++ comments/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def room(request, room_name):
-    return render(request, 'comments/room.html', {
-        'room_name': room_name
-    })
+    if not request.user.is_authenticated:
+        return redirect('/accounts/login')
 
+    return redirect('/accounts/login')
 
 def moderate(request, room_name):
+    if not request.user.is_authenticated:
+        return redirect('/accounts/login')
+
     return render(request, 'comments/room_mod.html', {
         'room_name': room_name
     })
--- swecha_comments/settings.py
+++ swecha_comments/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOGIN_REDIRECT_URL = '/lobby/moderate'
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'swecha_comments.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
