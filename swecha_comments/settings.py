8ef220feb8d7e4b382194db678a0a42c263abc35
MVS Sriharsha <sriharshamvs@gmail.com>
2021-01-01 20:31:15 +0530

    updated gitignore

--- swecha_comments/settings.py
+++ swecha_comments/settings.py
@@ -78,6 +78,8 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             "hosts": [('127.0.0.1', 6379)],
+            "capacity" : 3500,
+            "expiry": 10,
         },
     },
 }
